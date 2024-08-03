31364780ac8724797f14f8ba4ab03a8aa40b90ec
Seifeddine BENSALAH <seifeddine.bensalah@esprit.tn>
2024-08-03 12:31:43 +0100

    create view endpoints

--- backendCodingChallenge/backendCodingChallenge/views.py
+++ backendCodingChallenge/backendCodingChallenge/views.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
 from .models import Translation
-from .serializers import serializer
+from .serializers import TranslationSerializer
 
-def translation_list(request) :
+def translation_list(request):
     # get all translations 
     translations =Translation.objects.all()
-    serializer(translations,many=True)
-    return JsonResponse(serializer.data)
+    serializer =TranslationSerializer(translations,many=True)
+    return JsonResponse(serializer.data, safe=False)
