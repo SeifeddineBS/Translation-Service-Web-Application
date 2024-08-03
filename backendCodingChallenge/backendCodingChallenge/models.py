e3015f456a3135203d03b8e840a514fb7cdfba3c
Seifeddine BENSALAH <seifeddine.bensalah@esprit.tn>
2024-08-03 17:45:09 +0100

    add TextType as ENUM

--- backendCodingChallenge/backendCodingChallenge/models.py
+++ backendCodingChallenge/backendCodingChallenge/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 
 class Translation (models.Model) :
+
+    class TextType(models.TextChoices):
+        HTML = 'HTML', 'HTML'
+        PLAIN_TEXT = 'PLAIN_TEXT', 'PLAIN_TEXT'
+
+    type = models.CharField(
+        max_length=10,
+        choices=TextType.choices,
+        default=TextType.PLAIN_TEXT
+    )
+
     original_text = models.CharField(max_length=1000)
     translated_text = models.CharField(max_length=1000)
-    type = models.CharField(max_length=20)
     user = models.CharField(max_length=20)
     # add timestamp
 
