56f3bc0db8a5bf266b190262791c5cd7530834a3
Seifeddine BENSALAH <seifeddine.bensalah@esprit.tn>
2024-08-03 12:13:50 +0100

    create serializer

---
+++ backendCodingChallenge/backendCodingChallenge/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Translation
+class TranslationSerializer(serializers.ModelSerializer)
+    class Meta:
+        model = Translation
+        fields = ['id','original_text','translated_text','type','user']
