80c2d7ea7376c39905288131df3fb15b7d437e62
Seifeddine BENSALAH <seifeddine.bensalah@esprit.tn>
2024-08-04 15:59:25 +0100

    Add UserSerializer

--- backendCodingChallenge/backendCodingChallenge/serializers.py
+++ backendCodingChallenge/backendCodingChallenge/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 from .models import Translation
+from django.contrib.auth.models import User
+
 class TranslationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Translation
         fields = ['id','original_text','translated_text','type','user']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = User 
+        fields = ['id', 'username', 'password', 'email']
